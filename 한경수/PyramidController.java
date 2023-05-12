9c21c6396bec27a3fc86e461e98e65865d9d482b
hankyungsoo <hanks7431@naver.com>
2023-05-12 10:22:40 +0900

    피라미드 테스트 수정

--- 한경수/PyramidController.java
+++ 한경수/PyramidController.java
@@ -1,9 +1,11 @@
 /*
+
 PyramidController는 PyramidModel과 PyramidView를 관리하는 클래스입니다.
 run 메서드는 사용자 입력을 받고 처리하는 역할을 합니다.
 입력값을 검증하고 적절한 층수인지 확인합니다.
 유효한 층수일 경우 모델에 설정하고, 그렇지 않을 경우 에러 메시지를 출력합니다.
 마지막으로 PyramidView를 사용하여 피라미드를 출력합니다.
+
  */
 
 package Pyramid;
