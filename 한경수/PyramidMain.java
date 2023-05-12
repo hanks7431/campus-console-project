036b88054d606ee0d282ce300a2242b84b51c772
hankyungsoo <hanks7431@naver.com>
2023-05-12 15:17:54 +0900

    테스트 커밋 4차

--- 한경수/PyramidMain.java
+++ 한경수/PyramidMain.java
@@ -1,9 +1,7 @@
 /*
-
 PyramidMain은 프로그램의 시작점입니다.
 PyramidModel, PyramidView, PyramidController를 생성하고,
 컨트롤러의 run메서드를 호출하여 프로그램을 실행합니다.
-
  */
 package campus_console_project_leader_side.한경수;
 public class PyramidMain {
