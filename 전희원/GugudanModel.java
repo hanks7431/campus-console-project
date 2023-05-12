e0a6968b92539f2cc962824bd92f420ba2ec1029
DESKTOP-KAUMD0O\gram <wonxy@naver.com>
2023-05-12 15:59:22 +0900

    파일 삭제

--- 전희원/GugudanModel.java
+++
@@ -1,9 +0,0 @@
-public class GugudanModel {
-	public int[] run(int number) {
-        int[] result = new int[9];
-        for (int i = 0; i < 9; i++) {
-            result[i] = number * (i + 1);
-        }
-        return result;
-    }
-}
